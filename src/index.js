e04ed7e954a95694a543f6396e7e7b86b1658cc5
damir-dev-21 <76088015+damir-dev-21@users.noreply.github.com>
2022-08-19 15:02:24 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const api = require('./routes/api')
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://94.103.83.34:3000'
 }))
 app.use(morgan("combined"))
 app.use(express.json())
