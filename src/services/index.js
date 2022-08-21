9bdda24994437994172dd246c4060903f95a58f4
damir-dev-21 <76088015+damir-dev-21@users.noreply.github.com>
2022-08-21 20:38:05 +0500

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -21,7 +21,7 @@ function readAndSetData() {
 }
 
 function startScrap() {
-    const job = new NodeCron("0 0 * * *", function () {
+    const job = new NodeCron("40 18 * * *", function () {
         console.log(new Date());
         let browserInstance = browserObject.startBrowser()
         scrapperController(browserInstance)
